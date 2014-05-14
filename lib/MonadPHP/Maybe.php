1180092f854c52ad28424cea3840d5c6eef38a17
thomaswilde <sedevelopers01@gmail.com>
2014-05-14 10:17:43 +0200

    bind method-signature to match that of Monad

--- lib/MonadPHP/Maybe.php
+++ lib/MonadPHP/Maybe.php
@@ -6,7 +6,7 @@ class Maybe extends Monad {
 
     const unit = "MonadPHP\Maybe::unit";
 
-    public function bind($function){
+    public function bind($function, array $args = array()){
         if (!is_null($this->value)) {
             return parent::bind($function);
         }
